4e7f780d78592548b8344f5c904d1c15c6237210
oleg.k <oleg.k@sunrise-r.ru>
2016-04-18 15:59:49 +0300

    first commit

---
+++ scripts/bind.js
@@ -0,0 +1,18 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    //модуль по очистке данных на странице с помощью JavaScript
+    $('.timer-clear').bind('click', function() {
+        
+    }); 
+    //модуль по очистке данных на странице с помощью jQuery
+    $('.timer-empty').bind('click', function() {
+        
+    }); 
+});
+
+
---
+++ scripts/timer200.js
@@ -0,0 +1,28 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function(){
+    var timer2 = function(){
+        var i = 0;
+        for (i = 100; i <= 200; i++) {
+            console.log(i);
+            console.olog = function() {};
+            console.log = function(message) {
+                console.olog(message);
+                if (i % 2 == 0){
+                    $('#timer2').append( message + ' ');
+                }
+                else {
+                    $('#timer2').append('<span>' + message+ ' ' + '</span>');
+                }
+            };
+        }
+        $('#timer2').find('span').css({"color": "blue"});
+    }
+    timer2();
+});
+
+
